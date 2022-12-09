64ce696c24640948ba0df08c6d80260da586ed46
kyoungae <kyoungae3017@gmail.com>
2022-12-09 18:04:08 +0900

    Initial commit

---
+++ app/src/main/java/com/kyoungae/myownreastaurant/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.kyoungae.myownreastaurant
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kyoungae.myownreastaurant.ui.theme.MyOwnReastaurantTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyOwnReastaurantTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyOwnReastaurantTheme {
+        Greeting("Android")
+    }
+}
